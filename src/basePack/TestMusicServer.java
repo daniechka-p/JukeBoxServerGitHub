4d904ca2cab7459b90e486479d3a024cd5e5d589
Daniechka <paliy.d.sander@gmail.com>
2017-08-01 19:06:30 +0300

    Initial commit

---
+++ src/basePack/TestMusicServer.java
@@ -0,0 +1,11 @@
+package basePack;
+
+/**
+ * Created by Daniil on 31.07.2017.
+ */
+public class TestMusicServer {
+
+    public static void main(String[] args) {
+        new MusicServer().go();
+    }
+}
